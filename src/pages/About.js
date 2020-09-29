a5fa49f14a9b83d29759ef71f4e638a83af1ed19
John Martin <jrmartin182@gmail.com>
2020-09-29 09:31:45 -0600

    added codewars badge to about me

--- src/pages/About.js
+++ src/pages/About.js
@@ -9,10 +9,9 @@ const About = () => {
         <div>
             <div id="profile_container">
                 <div id="profile_img_container">
-                    <img src={Profile} alt="Profile image of John." />
+                    <img id="proPic" src={Profile} alt="Profile image of John." />
                     <h2 id="author">John Martin</h2>
                     <p id="profile_summary">Hi, my name is John and I am a JavaScript Web Developer based out of Salt Lake City. From racing in triathlons, teaching English in Asia and working in the fast paced world of fulfillment, my diverse background has built a quiver of skillsets that make me a well-rounded developer. Here you will find some of my projects along with my contact information should you be interested in connecting!</p>
-                    
 
                 </div>
             </div>
@@ -29,6 +28,12 @@ const About = () => {
                     <li>AWS in progress...</li>
                 </ul>
                 <p id="resume"><Link to="/resume">View my resume.</Link></p>
+                <div id="badge">
+                    <a href="https://www.codewars.com/users/JohnnyBEdge">
+                        <img id="codewars" src="https://www.codewars.com/users/JohnnyBEdge/badges/large" alt="CodeWars.com badge."/>
+                    </a>
+                </div>
+                
             </div>
         </div>
     )
